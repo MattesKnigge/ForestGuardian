44ea1b98cc2bbac3cc86224c6f685b5d8c450d27
Julian Schöpe <julian.schoepe@web.de>
2023-11-11 13:04:12 +0100

    added random details

--- django_server/sensorknoten_vogelhaus/views.py
+++ django_server/sensorknoten_vogelhaus/views.py
@@ -22,24 +22,28 @@ def get_location(request, location_name: str):
     if location_name == "random":
         data = {
             'temperature': {
+                'id': 'random_temperature',
                 'timestamp': datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S"),
                 'value': random.randint(-25, 42),
                 'min': -25,
                 'max': 42,
             },
             'humidity': {
+                'id': 'random_humidity',
                 'timestamp': datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S"),
                 'value': random.randint(0, 100),
                 'min': 0,
                 'max': 100,
             },
             'pressure': {
+                'id': 'random_pressure',
                 'timestamp': datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S"),
                 'value': random.randint(800, 1100),
                 'min': 800,
                 'max': 1100,
             },
             'air_quality': {
+                'id': 'random_airQuality',
                 'timestamp': datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S"),
                 'value': random.randint(400, 60000),
                 'min': 400,
@@ -65,15 +69,30 @@ def get_location(request, location_name: str):
 
 @api_view(['GET'])
 def get_measured_parameter_details(request, measured_parameter_id: str):
-    mp = MeasuredParameter.objects.select_related('parameter', 'sensor').get(id=measured_parameter_id)
-    values = SensorValue.objects.filter(measuredParameter=mp).all()  # how many is max count?
-    data = {
-        'name': mp.parameter.name,
-        'sensor': mp.sensor.name,
-        'parameter_description': mp.parameter.description,
-        'sensor_description': mp.sensor.description,
-        'values': [{'timestamp': time.mktime(v.created_at.timetuple()), 'value': v.value} for v in values]
-    }
+    if measured_parameter_id.startswith('random'):
+        param = measured_parameter_id.split('_')
+        min_max = {'temperature': {'min': -25, 'max': 42}, 'humidity': {'min': 0, 'max': 100},
+                   'pressure': {'min': 800, 'max': 1100}, 'airQuality': {'min': 400, 'max': 60000}}
+        mm = min_max.get(param[1])
+        timestamps = [datetime.datetime.now() - datetime.timedelta(days=20 - i) for i in range(20)]
+        data = {
+            'name': param[1],
+            'sensor': 'Random Sensor',
+            'parameter_description': 'Random Sensor Description',
+            'sensor_description': 'Random Parameter Description',
+            'values': [{'timestamp': time.mktime(timestamps[idx].timetuple())*1000, 'value': v} for idx, v in enumerate([random.randint(mm['min'], mm['max']) for i in range(20)])]
+        }
+    else:
+
+        mp = MeasuredParameter.objects.select_related('parameter', 'sensor').get(id=measured_parameter_id)
+        values = SensorValue.objects.filter(measuredParameter=mp).all()  # how many is max count?
+        data = {
+            'name': mp.parameter.name,
+            'sensor': mp.sensor.name,
+            'parameter_description': mp.parameter.description,
+            'sensor_description': mp.sensor.description,
+            'values': [{'timestamp': time.mktime(v.created_at.timetuple())*1000, 'value': v.value} for v in values]
+        }
 
     return Response(data)
 
