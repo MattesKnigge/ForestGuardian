b63255104286f7dfe339944582dad9c6e36cfe18
Julian Schöpe <julian.schoepe@web.de>
2023-11-16 12:13:05 +0100

    added admin page to add DB configuration

--- django_server/sensorknoten_vogelhaus/admin.py
+++ django_server/sensorknoten_vogelhaus/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Location, Sensor, Parameter, MeasuredParameter, SensorValue
 
 # Register your models here.
+admin.site.register(Location)
+admin.site.register(Sensor)
+admin.site.register(Parameter)
+admin.site.register(MeasuredParameter)
+admin.site.register(SensorValue)
--- django_server/sensorknoten_vogelhaus/models.py
+++ django_server/sensorknoten_vogelhaus/models.py
@@ -8,10 +8,16 @@ class Location(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=256, unique=True)
 
+    def __str__(self):
+        return self.name
+
 
 class Sensor(models.Model):
     name = models.CharField(max_length=256, unique=True)
     description = models.CharField(max_length=1024, unique=False)
+
+    def __str__(self):
+        return self.name
 
 
 class Parameter(models.Model):
@@ -22,11 +28,17 @@ class Parameter(models.Model):
     lower_good_bound = models.FloatField(blank=False)
     upper_good_bound = models.FloatField(blank=False)
 
+    def __str__(self):
+        return self.name
+
 
 class MeasuredParameter(models.Model):
     location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='loc_mp')
     sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
     parameter = models.ForeignKey(Parameter, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.location.name + " " + self.parameter.name + " " + self.sensor.name
 
 
 class SensorValue(models.Model):
@@ -34,4 +46,6 @@ class SensorValue(models.Model):
     value = models.FloatField(blank=False)
     measuredParameter = models.ForeignKey(MeasuredParameter, on_delete=models.CASCADE)
 
-
+    def __str__(self):
+        return self.measuredParameter.location.name + " " + self.measuredParameter.parameter.name + ": " \
+            + str(self.value) + " " + self.created_at.strftime("%d.%m.%Y %H:%M:%S")
