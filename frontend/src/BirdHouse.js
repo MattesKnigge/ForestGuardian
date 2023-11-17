a1ac9a15bd6691ad425b23ec4ed6df45153c7c19
Julian Schöpe <julian.schoepe@web.de>
2023-11-17 22:27:52 +0100

    added polling to update data whe new values arrive

--- frontend/src/BirdHouse.js
+++ frontend/src/BirdHouse.js
@@ -9,20 +9,40 @@ import Credits from "./components/Credits";
 const BirdHouse = ({ showMessage }) => {
     const {birdHouseName} = useParams();
     const [data, setData] = useState({"paramName": { timestamp: "", value: "-25", min: "-25", max: "100" }});
+    const [lastTimestamp, setLastTimestamp] = useState();
+    const pollingFrequency = 5 * 1000;
 
     useEffect(() => {
         async function fetchData() {
             try {
                 const response = await axios.get(`/sensorknoten-vogelhaus/location/${birdHouseName}`);
                 setData(response.data);
+                const timeResponse = await axios.get(`/sensorknoten-vogelhaus/location/${birdHouseName}/latest`);
+                setLastTimestamp(timeResponse.data);
             } catch (error) {
                 console.dir(error);
                 showMessage('An error occurred while fetching data.', 'error');
             }
         }
-
+        
         fetchData();
     }, [birdHouseName, showMessage]);
+
+    const doPolling = async () => {
+        try {
+            const timeResponse = await axios.get(`/sensorknoten-vogelhaus/location/${birdHouseName}/latest`);
+            if (timeResponse.data > lastTimestamp) {
+                setLastTimestamp(timeResponse.data);
+                const response = await axios.get(`/sensorknoten-vogelhaus/location/${birdHouseName}`);
+                setData(response.data);
+            }
+        } catch (error) {
+            console.dir(error);
+            showMessage('An error occurred while fetching data.', 'error');
+        }
+        setTimeout(doPolling, pollingFrequency);
+    }
+    setTimeout(doPolling, pollingFrequency);
 
     return(
         <div className="layout">
