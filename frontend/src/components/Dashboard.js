ad1f3348bac758a89049a74c717a2a26166dc610
Julian Schöpe <julian.schoepe@web.de>
2023-11-29 12:35:59 +0100

    read dash unit from json

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -28,13 +28,13 @@ const Dashboard = ({ sensors }) => {
                     <h3 style={{fontFamily: 'Dosis, sans-serif'}}>{sensorNames[key] || key}</h3>
                     <div key={key} className="dashboard-row">
                         {key === "temperature" ? (
-                            <GaugeComponent data={sensors[key]} arcs={[20 / 67, 35 / 67, 12 / 67]} colours={["#EA4228", "#4cda15", "#EA4228"]} unit={" °C"} />
+                            <GaugeComponent data={sensors[key]} arcs={[20 / 67, 35 / 67, 12 / 67]} colours={["#EA4228", "#4cda15", "#EA4228"]} />
                         ) : key === "humidity" ? (
-                            <GaugeComponent data={sensors[key]} colours={["#EA4228", "#4cda15", "#4cda15"]} unit={" %"} />
+                            <GaugeComponent data={sensors[key]} colours={["#EA4228", "#4cda15", "#4cda15"]} />
                         ) : key === "pressure" ? (
-                             <GaugeComponent data={sensors[key]} padding={0} nrLevels={20} colours={["#6699CC", "#993333"]} unit={" hPa"} />
+                             <GaugeComponent data={sensors[key]} padding={0} nrLevels={20} colours={["#6699CC", "#993333"]} />
                         ) : key === "air_quality" ? (
-                            <GaugeComponent data={sensors[key]} colours={["#4cda15", "#F8C630", "#EA4228"]} unit={" ppm"} />
+                            <GaugeComponent data={sensors[key]} colours={["#4cda15", "#F8C630", "#EA4228"]} />
                         ) : null}
                         <ChartComponent measured_parameter_id={sensors[key].id} from={from} to={to} />
                     </div>
