84743bdd3c5b855c0eac2da995c1c060ba899b54
Mattes <m.knigge@ostfalia.de>
2023-11-29 12:37:10 +0100

    styling

--- frontend/src/components/ParamCard.js
+++ frontend/src/components/ParamCard.js
@@ -1,20 +1,26 @@
 import {sensorNames} from "../util/utils";
+import Button from "@mui/material/Button";
 
 const ParamCard = ({name, data}) => {
 
     return (
         <div className="param-card">
-            <h3>{sensorNames[name]}</h3>
+            <h1>{sensorNames[name]}</h1>
             <div className="param-card-content">
-                <div>
+                <div className="card-value">
                     {data.value}
+                    <div className="card-unit">
+                        {data.unit}
+                    </div>
                 </div>
                 {name === "temperature" ?
                     <div className="param-svg bird-cold" />
                     : <div></div>}
             </div>
             <div>
-                TODO: Button for more details
+                <Button class="info-button">
+                    More Info
+                </Button>
             </div>
         </div>
     );
