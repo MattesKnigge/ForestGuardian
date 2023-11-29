6c0723b1281403f4998c010332fb4f60a2fdbd10
Mattes <m.knigge@ostfalia.de>
2023-11-29 14:15:15 +0100

    worked on ParamCard.js and DetailedView.js

--- frontend/src/components/ParamCard.js
+++ frontend/src/components/ParamCard.js
@@ -2,10 +2,15 @@ import {sensorNames} from "../util/utils";
 import Button from "@mui/material/Button";
 
 const ParamCard = ({name, data}) => {
+    const handleButtonClick = () => {
+        // Add your click handling logic here
+        console.log('More Info clicked');
+        //TODO: Open Detail.js on click
+    };
 
     return (
         <div className="param-card">
-            <h1>{sensorNames[name]}</h1>
+            <h1 className="param-card-title">{sensorNames[name]}</h1>
             <div className="param-card-content">
                 <div className="card-value">
                     {data.value}
@@ -16,7 +21,7 @@ const ParamCard = ({name, data}) => {
                 <div className={`param-svg bird-${data.value_range.tag}`} />
             </div>
             <div>
-                <Button class="info-button">
+                <Button class="more-info-button" onClick={handleButtonClick}>
                     More Info
                 </Button>
             </div>
@@ -25,3 +30,4 @@ const ParamCard = ({name, data}) => {
 }
 
 export default ParamCard;
+
