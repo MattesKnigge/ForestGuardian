4b7b7594e69c866f4415341b056d8b66128428dc
Julian Schöpe <julian.schoepe@web.de>
2023-12-08 09:48:27 +0100

    added file upload

--- django_server/sensorknoten_vogelhaus/urls.py
+++ django_server/sensorknoten_vogelhaus/urls.py
@@ -9,6 +9,7 @@ from . import views_post
 urlpatterns = [
     path('locations', views_get.locations, name='locations'),
     path('location/data', views_post.location_data, name='post_location_data'),
+    path('location/image', views_post.location_image, name='post_location_image'),
     path('location/<location_name>', views_get.location, name='location'),
     path('location/<location_name>/latest', views_get.latest_sensor_value_timestamp, name='location_last_timestamp'),
     path('measured_parameter/<measured_parameter_id>', views_get.measured_parameter_details, name='mp_details'),
--- django_server/sensorknoten_vogelhaus/views_post.py
+++ django_server/sensorknoten_vogelhaus/views_post.py
@@ -1,29 +1,30 @@
+import datetime
+import base64
+import os
+
+from django.conf import settings
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .models import Location, MeasuredParameter, SensorValue
+from .models import Location, MeasuredParameter, SensorValue, LocationImage
 
 
 @swagger_auto_schema(request_body=openapi.Schema(
     type=openapi.TYPE_OBJECT,
     properties={
         'values': openapi.Schema(type=openapi.TYPE_OBJECT,
-                                 description='dictionary of all parameters with their values',
+                                 description='dictionary of timestamps in seconds from epoch each with the values as object',
                                  properties={
-                                     'temperature': openapi.Schema(type=openapi.TYPE_ARRAY,
-                                                                   items=openapi.Schema(
-                                                                       type=openapi.TYPE_NUMBER)),
-                                     'humidity': openapi.Schema(type=openapi.TYPE_ARRAY,
-                                                                items=openapi.Schema(
-                                                                    type=openapi.TYPE_NUMBER)),
-                                     'pressure': openapi.Schema(type=openapi.TYPE_ARRAY,
-                                                                items=openapi.Schema(
-                                                                    type=openapi.TYPE_NUMBER)),
-                                     'air_quality': openapi.Schema(type=openapi.TYPE_ARRAY,
-                                                                   items=openapi.Schema(
-                                                                       type=openapi.TYPE_NUMBER)),
+                                     '000000': openapi.Schema(
+                                         type=openapi.TYPE_OBJECT,
+                                         properties={
+                                             'temperature': openapi.Schema(type=openapi.TYPE_NUMBER),
+                                             'humidity': openapi.Schema(type=openapi.TYPE_NUMBER),
+                                             'pressure': openapi.Schema(type=openapi.TYPE_NUMBER),
+                                             'air_quality': openapi.Schema(type=openapi.TYPE_NUMBER),
+                                         })
                                  })
     },
 ), method='POST')
@@ -42,5 +43,29 @@ def location_data(request):
                 values.append(SensorValue(value=v, measuredParameter=mp))
 
     SensorValue.objects.bulk_create(values)
+
+    return Response(status=status.HTTP_200_OK)
+
+
+@swagger_auto_schema(request_body=openapi.Schema(
+    type=openapi.TYPE_OBJECT,
+    properties={
+        '000000': openapi.Schema(type=openapi.TYPE_STRING, description='image in base64 encoding')
+    },
+), method='POST')
+@api_view(['POST'])
+def location_image(request):
+    loc = Location.objects.get(name='prototype')
+    data = request.data
+    for timestamp, image_as_base64 in data.items():
+        filename = loc.name + '_' + timestamp + '.png'
+        with open(os.path.join(settings.MEDIA_ROOT, filename), "xb") as binary_file:
+            binary_file.write(base64.b64decode(image_as_base64))
+
+        LocationImage.objects.create(
+            location=loc,
+            created_at=datetime.datetime.utcfromtimestamp(int(timestamp)),
+            image=filename,
+        )
 
     return Response(status=status.HTTP_200_OK)
