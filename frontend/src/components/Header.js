1d00b6d6ad9ecba59bdc012104d0333f1a5b30da
Mattes <m.knigge@ostfalia.de>
2023-12-15 21:39:45 +0100

    Added a Button to show a map with the exact location of the ForestGuardian

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -5,12 +5,35 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import InfoDialog from "./InfoDialog";
 import QuestionMarkRoundedIcon from '@mui/icons-material/QuestionMarkRounded';
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
-import DynamicFeedRoundedIcon from '@mui/icons-material/DynamicFeedRounded';import { useNavigate } from 'react-router-dom';
+import DynamicFeedRoundedIcon from '@mui/icons-material/DynamicFeedRounded';
+import PublicRoundedIcon from '@mui/icons-material/PublicRounded';
 import Typography from "@mui/material/Typography";
-import {brown, green} from "../util/utils";
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import { useNavigate } from 'react-router-dom';
+import {MapContainer, TileLayer, Marker, Popup} from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+import { brown, green } from "../util/utils";
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+import L from 'leaflet';
+
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    tooltipAnchor: [16, -28],
+    shadowSize: [41, 41],
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
 
 const Header = ({ onToggleClick, toggleOn, showToggle = false, weather }) => {
     const [isInfoOpen, setIsInfoOpen] = useState(false);
+    const [isMapDialogOpen, setIsMapDialogOpen] = useState(false);
     const navigate = useNavigate();
 
     const buttonStyle = {
@@ -46,7 +69,15 @@ const Header = ({ onToggleClick, toggleOn, showToggle = false, weather }) => {
 
     const handleMenuClick = () => {
         navigate('/bird-house/min,random,max');
-    }
+    };
+
+    const handleMapClick = () => {
+        setIsMapDialogOpen(true);
+    };
+
+    const handleMapDialogClose = () => {
+        setIsMapDialogOpen(false);
+    };
 
     const titleStyle = {
         color: green,
@@ -61,6 +92,15 @@ const Header = ({ onToggleClick, toggleOn, showToggle = false, weather }) => {
         marginRight: '2rem',
     };
 
+    // TODO: Get marker coordinates from backend
+    const markerLat = 52.180678;
+    const markerLng = 10.556785;
+
+    const mapContainerStyle = {
+        height: '500px',
+        width: '100%',
+    };
+
     return (
         <header className="header">
             <div className="header-content">
@@ -68,15 +108,14 @@ const Header = ({ onToggleClick, toggleOn, showToggle = false, weather }) => {
             </div>
             <div className="weather_description" style={weatherDescriptionStyle}>
                 {weather ? "Weather Description: " + weather.name : null}
-
             </div>
             <div className="header-button">
                 {showToggle && (
                     <div style={buttonContainerStyle}>
                         <FormControlLabel
-                            control={<Switch Switch checked={toggleOn} onChange={onToggleClick} style={{ color: brown }} title={'Toggle dashboard view'} />}
+                            control={<Switch checked={toggleOn} onChange={onToggleClick} style={{ color: brown }} title={'Toggle dashboard view'} />}
                             label={
-                                <Typography variant="body2" style={{fontFamily: 'Dosis, sans-serif', color: green, fontSize: '1.3em', fontWeight: 'bold'}}>
+                                <Typography variant="body2" style={{ fontFamily: 'Dosis, sans-serif', color: green, fontSize: '1.3em', fontWeight: 'bold' }}>
                                     Dashboard
                                 </Typography>
                             }
@@ -99,7 +138,6 @@ const Header = ({ onToggleClick, toggleOn, showToggle = false, weather }) => {
                 >
                     <QuestionMarkRoundedIcon />
                 </IconButton>
-                {/*TODO: display menu with all birdhouses to choose from*/}
                 <IconButton
                     sx={buttonStyle}
                     onClick={handleMenuClick}
@@ -107,8 +145,34 @@ const Header = ({ onToggleClick, toggleOn, showToggle = false, weather }) => {
                 >
                     <DynamicFeedRoundedIcon />
                 </IconButton>
+                {window.location.href.includes("bird-house") &&
+                    <IconButton
+                        sx={buttonStyle}
+                        onClick={handleMapClick}
+                        title={'Show Birdhouse Map'}
+                    >
+                        <PublicRoundedIcon />
+                    </IconButton>
+                }
             </div>
             <InfoDialog open={isInfoOpen} onClose={handleCloseInfo} />
+
+            <Dialog open={isMapDialogOpen} onClose={handleMapDialogClose} maxWidth="lg" fullWidth>
+                <DialogContent>
+                    <div id="mapDialog" style={mapContainerStyle}>
+                        <MapContainer center={[markerLat, markerLng]} zoom={17} style={{ height: '100%', width: '100%' }}>
+                            <TileLayer
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            />
+                            <Marker position={[markerLat, markerLng]}>
+                                <Popup>
+                                    Position of your ForestGuardian.
+                                </Popup>
+                            </Marker>
+                        </MapContainer>
+                    </div>
+                </DialogContent>
+            </Dialog>
         </header>
     );
 };
