bc1d354dbec401bb4c781b7d2ac30efdf7d0d226
Julian Schöpe <julian.schoepe@web.de>
2023-12-15 07:09:57 +0100

    fix post parsing

--- django_server/sensorknoten_vogelhaus/views_post.py
+++ django_server/sensorknoten_vogelhaus/views_post.py
@@ -14,18 +14,19 @@ from .models import Location, MeasuredParameter, SensorValue, LocationImage
 @swagger_auto_schema(request_body=openapi.Schema(
     type=openapi.TYPE_OBJECT,
     properties={
-        'values': openapi.Schema(type=openapi.TYPE_OBJECT,
-                                 description='dictionary of timestamps in seconds from epoch each with the values as object',
-                                 properties={
-                                     '000000': openapi.Schema(
-                                         type=openapi.TYPE_OBJECT,
-                                         properties={
-                                             'temperature': openapi.Schema(type=openapi.TYPE_NUMBER),
-                                             'humidity': openapi.Schema(type=openapi.TYPE_NUMBER),
-                                             'pressure': openapi.Schema(type=openapi.TYPE_NUMBER),
-                                             'air_quality': openapi.Schema(type=openapi.TYPE_NUMBER),
-                                         })
-                                 })
+        'values': openapi.Schema(
+            type=openapi.TYPE_OBJECT,
+            description='dictionary of timestamps in seconds from epoch each with the values as object',
+            properties={
+                '000000': openapi.Schema(
+                    type=openapi.TYPE_OBJECT,
+                    properties={
+                        'temperature': openapi.Schema(type=openapi.TYPE_NUMBER),
+                        'humidity': openapi.Schema(type=openapi.TYPE_NUMBER),
+                        'pressure': openapi.Schema(type=openapi.TYPE_NUMBER),
+                        'air_quality': openapi.Schema(type=openapi.TYPE_NUMBER),
+                    })
+            })
     },
 ), method='POST')
 @api_view(['POST'])
@@ -39,10 +40,9 @@ def location_data(request):
     for timestamp, vals in data['values'].items():
         dt = datetime.datetime.utcfromtimestamp(int(timestamp))
         for mp in mps:
-            sent_vals = vals.get(mp.parameter.name, None)
-            if sent_vals is not None:
-                for v in sent_vals:
-                    values.append(SensorValue(value=v, measuredParameter=mp, created_at=dt))
+            sent_val = vals.get(mp.parameter.name, None)
+            if sent_val is not None:
+                values.append(SensorValue(value=sent_val, measuredParameter=mp, created_at=dt))
 
     SensorValue.objects.bulk_create(values)
 
