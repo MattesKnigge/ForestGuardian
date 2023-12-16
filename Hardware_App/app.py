6654ce5c81c1be1ae05d1fb5d184b4b13dcad805
Niklas Lugowski <n.lugowski@ostfalia.de>
2023-12-16 21:48:15 +0100

    Update IP address and port for server and fix timestamps

--- Hardware_App/app.py
+++ Hardware_App/app.py
@@ -3,9 +3,10 @@ import bme280
 import smbus
 import time
 import requests
+import datetime
 
 # Define the IP address and port for the server where sensor data will be sent
-ip_addr = "http://192.168.235.52:8080"
+ip_addr = "http://192.168.167.52:8000"
 port = 1
 
 # I2C addresses for the BME280 and ISL29125 sensors
@@ -17,9 +18,10 @@ bus = smbus.SMBus(port)
 
 # Define a global variable to store sensor data
 global sensor_data
+global sensor_dict
 global sensor_data_dict
 sensor_data_dict = {}
-
+sensor_dict = {}
 # Set a placeholder, used when Sensor-values are not available
 placeholder = "N/A"
 sensor_data = placeholder
@@ -28,26 +30,9 @@ sensor_data = placeholder
 global iteration_count
 iteration_count = 0
 
-# Function to read RGB color values from the ISL29125 sensor
-def read_color_values(bus, addr):
-    # Trigger a measurement on the ISL29125
-    bus.write_byte(addr, 0x00)
-    sleep(0.5)
-    # Read the RGB color values
-    data = bus.read_i2c_block_data(addr, 0x09, 3)
-    return data[0], data[1], data[2]
-
 # Main loop to continuously read and send sensor data
 def loop():
     while True:
-        try:
-            # Read RGB color values from the ISL29125 sensor
-            red, green, blue = read_color_values(bus, isl29125_addr)
-        except:
-            red = placeholder
-            green = placeholder
-            blue = placeholder
-
         try:
             # Load calibration parameters and read data from the BME280 sensor
             bme280.load_calibration_params(bus, bme280_addr)
@@ -59,39 +44,39 @@ def loop():
             humidity = placeholder
             pressure = placeholder
             ambient_temperature = placeholder
-
-        # Update the sensor data with the humidity value
-        sensor_data = humidity
         
-        # Create a dict with sensor data
-        sensor_dict = {
-            "sensors": {
-                "Humidity": humidity,
-                "Pressure": pressure,
-                "Temperature": ambient_temperature,
-                "Red": red,
-                "Green": green,
-                "Blue": blue
-            }
+        # Store the sensor data in the dictionary with the current Linux timestamp as the key
+        sensor_dict[int(datetime.datetime.utcnow().timestamp())] = {
+                "humidity": humidity,
+                "pressure": pressure,
+                "ambient_temperature": ambient_temperature,
+                "co2": 500,
+                "co": 2,
+                "o2": 3,
+                "light": 10000 
         }
 
-        # Store the sensor data in the dictionary with the current Linux timestamp as the key
-        sensor_data_dict[int(time.time())] = sensor_dict
+        sensor_data_dict={
+            "values":sensor_dict
+        }
         
         global iteration_count
-
+        print(iteration_count)
         # only send data every 6 hours
         if iteration_count >= 11:
             # Send the sensor data to the specified server using an HTTP POST request
-            requests.post(url=f"{ip_addr}/location/data", json=sensor_data_dict)
+            requests.post(url=f"{ip_addr}/sensorknoten-vogelhaus/location/data", json=sensor_data_dict)
+            sensor_data_dict.clear()
+            sensor_dict.clear()
             # Reset the iteration count
             iteration_count = 0
 
-	# increment the iteration count
+	    # increment the iteration count
         iteration_count+=1
         
         # Sleep for 30 minutes before the next iteration
-        time.sleep(1800)
+        time.sleep(1)
+
 
 if __name__ == "__main__":
     # Iterate through I2C addresses to find connected sensors
