869958b6cb454e980d986682f26d805c3d0dd34d
Mattes <m.knigge@ostfalia.de>
2023-12-17 12:11:48 +0100

    Added QR-Code generation

--- frontend/src/components/Credits.js
+++ frontend/src/components/Credits.js
@@ -1,4 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
+import QRCode from 'qrcode.react';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography'; // Import Typography
+import { green, brown, gold } from "../util/utils";
 
 const contributors = [
     { name: 'Niklas Lugowski', githubUsername: 'NiklasLugi' },
@@ -14,7 +22,29 @@ const handleGitHubLinkClick = (githubUsername) => {
     window.open(`${githubURL}${githubUsername}`, '_blank');
 };
 
+const hexToRgb = (hex) => {
+    const bigint = parseInt(hex.slice(1), 16);
+    const r = (bigint >> 16) & 255;
+    const g = (bigint >> 8) & 255;
+    const b = bigint & 255;
+    return [r, g, b];
+};
+
+const rgbGreen = hexToRgb(green);
+
 const Credits = () => {
+    const [qrCodeData, setQrCodeData] = useState('');
+    const [openDialog, setOpenDialog] = useState(false);
+
+    const handleGenerateQRCodeClick = (data) => {
+        setQrCodeData(data);
+        setOpenDialog(true);
+    };
+
+    const handleCloseDialog = () => {
+        setOpenDialog(false);
+    };
+
     const contributorsList = contributors.map((contributor, index) => (
         <span key={index} className="credit-link" onClick={() => handleGitHubLinkClick(contributor.githubUsername)}>
             {contributor.name}{index < contributors.length - 1 ? ', ' : ''}
@@ -32,9 +62,63 @@ const Credits = () => {
                     |{' '}
                     <a className="country" href="https://5g-smart-country.de" target="_blank" rel="noreferrer">
                         5G Smart Country
-                    </a>{' '}
+                    </a>{' | '}
+                    <span className="country" onClick={() => handleGenerateQRCodeClick(window.location.href)}>
+                        Generate QR-Code
+                    </span>{' '}
                     | 2023
                 </p>
+                <Dialog
+                    open={openDialog}
+                    onClose={handleCloseDialog}
+                    PaperProps={{
+                        style: {
+                            backgroundColor: `rgba(${rgbGreen.join(', ')}, 0.7)`,
+                            color: gold,
+                            borderRadius: '15px',
+                            backdropFilter: 'blur(5px)',
+                            width: 'fit-content',
+                            height: 'fit-content',
+                            padding: '1rem',
+                            margin: '1rem',
+                            display: 'flex',
+                            flexDirection: 'column',
+                            alignItems: 'center',
+                            justifyContent: 'center',
+                        },
+                    }}
+                >
+                    <DialogTitle>
+                        <Typography variant="h6" style={{ fontFamily: 'Dosis', fontSize: '2rem' }}>
+                            Your Guardian
+                        </Typography>
+                    </DialogTitle>
+                    <DialogContent>
+                        <DialogContentText style={{ color: brown, fontFamily: 'Dosis', textAlign: 'center' }}>
+                            Scan the code below to share
+                        </DialogContentText>
+                        <QRCode
+                            value={qrCodeData}
+                            style={{
+                                marginTop: '1rem',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                display: 'flex',
+                            }}
+                            fgColor={"#E2DFD2"}
+                            bgColor={`rgba(${rgbGreen.join(', ')}, 0)`}
+                            level={"H"}
+                            size={256}
+                            imageSettings={{
+                                src: '/temperature_average.svg',
+                                height: 200,
+                                width: 350,
+                                excavate: false,
+                            }}
+                        />
+                    </DialogContent>
+                    <Button onClick={handleCloseDialog} style={{ color: brown, fontFamily: 'Bebas Neue' }}>Close</Button>
+                </Dialog>
             </div>
         </div>
     );
